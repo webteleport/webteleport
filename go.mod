c1a0dcc2e05c09b73e141ab735efd1e36e43c8bb
navigaid <navigaid@gmail.com>
2022-09-23 13:00:27 +0000

    log listening port

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/btwiuse/quichost
 
 go 1.19
+
+require github.com/btwiuse/h3 v0.0.0-20220919172148-51b6e2f27e33
--- server/server.go
+++ server/server.go
@@ -5,10 +5,14 @@ import (
 	"log"
 	"net"
 	"net/http"
+
+	"github.com/btwiuse/h3/utils"
 )
 
 func main() {
-	ln, err := net.Listen("tcp4", ":3000")
+	port := utils.EnvPort(":3000")
+	log.Println("listening on https://127.0.0.1" + port)
+	ln, err := net.Listen("tcp4", port)
 	if err != nil {
 		log.Fatalln(err)
 	}
