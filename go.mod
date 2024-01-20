d52702a241f96667c012a01a233b4278f40aba33
navigaid <navigaid@gmail.com>
2024-01-20 07:28:42 +0000

    refactor Resolve

--- go.mod
+++ go.mod
@@ -1,13 +1,13 @@
 module github.com/webteleport/webteleport
 
-go 1.21
+go 1.21.6
 
 // replace github.com/webteleport/utils => ../utils
 
 require (
-	github.com/quic-go/quic-go v0.40.1
+	github.com/quic-go/quic-go v0.41.0
 	github.com/quic-go/webtransport-go v0.6.0
-	github.com/webteleport/utils v0.2.1
+	github.com/webteleport/utils v0.2.2
 )
 
 require (
@@ -17,17 +17,16 @@ require (
 	github.com/google/pprof v0.0.0-20230821062121-407c9e7a662f // indirect
 	github.com/gorilla/handlers v1.5.2 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
-	github.com/miekg/dns v1.1.57 // indirect
+	github.com/miekg/dns v1.1.58 // indirect
 	github.com/onsi/ginkgo/v2 v2.12.0 // indirect
 	github.com/quic-go/qpack v0.4.0 // indirect
-	github.com/quic-go/qtls-go1-20 v0.4.1 // indirect
 	github.com/rs/cors v1.10.1 // indirect
 	go.uber.org/mock v0.3.0 // indirect
-	golang.org/x/crypto v0.16.0 // indirect
-	golang.org/x/exp v0.0.0-20240103183307-be819d1f06fc // indirect
+	golang.org/x/crypto v0.18.0 // indirect
+	golang.org/x/exp v0.0.0-20240119083558-1b970713d09a // indirect
 	golang.org/x/mod v0.14.0 // indirect
-	golang.org/x/net v0.19.0 // indirect
-	golang.org/x/sys v0.15.0 // indirect
+	golang.org/x/net v0.20.0 // indirect
+	golang.org/x/sys v0.16.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
-	golang.org/x/tools v0.16.0 // indirect
+	golang.org/x/tools v0.17.0 // indirect
 )
--- resolve.go
+++ resolve.go
@@ -10,6 +10,39 @@ import (
 	"github.com/webteleport/utils"
 )
 
+// Resolve gets all webteleport endpoints from Alt-Svc dns records / headers
+func Resolve(u *url.URL) (endpoints []string) {
+	endpoints = append(endpoints, eps(TXT(u.Host))...)
+	endpoints = append(endpoints, eps(HEAD(u.String()))...)
+	return
+}
+
+func eps(altsvcs []string) (endpoints []string) {
+	for _, v := range altsvcs {
+		es := utils.ExtractAltSvcEndpoints(v, "webteleport")
+		endpoints = append(endpoints, es...)
+	}
+	return
+}
+
+// HEAD gets all altsvcs from Alt-Svc header values of given url
+func HEAD(s string) []string {
+	resp, err := http.Head(s)
+	if err != nil {
+		slog.Warn(fmt.Sprintf("http head error: %v", err))
+	}
+	return resp.Header.Values("Alt-Svc")
+}
+
+// TXT gets all altsvcs from TXT records of given host
+func TXT(h string) []string {
+	txts, err := utils.LookupHostTXT(h, "1.1.1.1:53")
+	if err != nil {
+		slog.Warn(fmt.Sprintf("dns lookup error: %v", err))
+	}
+	return altsvcLines(txts)
+}
+
 func altsvcLines(txts []string) []string {
 	const prefix = "Alt-Svc: "
 
@@ -24,24 +57,4 @@ func altsvcLines(txts []string) []string {
 	}
 
 	return altsvcs
-}
-
-// Resolve gets all webteleport endpoints from Alt-Svc dns records / headers
-func Resolve(u *url.URL) (endpoints []string) {
-	resp, err := http.Head(u.String())
-	if err != nil {
-		slog.Warn(fmt.Sprintf("http head error: %v", err))
-	}
-	altsvcHeader := resp.Header.Get("Alt-Svc")
-
-	txts, err := utils.LookupHostTXT(u.Host, "1.1.1.1:53")
-	if err != nil {
-		slog.Warn(fmt.Sprintf("dns lookup error: %v", err))
-	}
-	altsvcs := append(altsvcLines(txts), altsvcHeader)
-	// log.Println(altsvcs)
-	for _, altsvc := range altsvcs {
-		endpoints = append(endpoints, utils.ExtractAltSvcEndpoints(altsvc, "webteleport")...)
-	}
-	return
 }
